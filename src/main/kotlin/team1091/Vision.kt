65d8e16921bb9662396617f66e6891f23c0943aa
Team1091 <frcteam1091@gmail.com>
2022-02-12 10:33:24 -0600

    Seperated things and cleaned it up

--- src/main/kotlin/team1091/Vision.kt
+++ src/main/kotlin/team1091/Vision.kt
@@ -8,7 +8,6 @@ import com.github.sarxos.webcam.ds.ipcam.IpCamMode
 import edu.wpi.first.networktables.NetworkTableEntry
 import edu.wpi.first.networktables.NetworkTableInstance
 import kotlinx.coroutines.runBlocking
-import spark.Spark.exception
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics2D
@@ -22,12 +21,9 @@ import javax.swing.WindowConstants
 import kotlin.math.abs
 import kotlin.math.pow
 
-
-private var imageInfo = ImageInfo()
-
 private val testImage: String? = null // "test.png"
 private const val robotAddr = "http://roborio-1091-frc.local" // "http://localhost"//
-var remote = false
+var isRemote = false
 var teamColor = "blue"
 
 var seenEntry: NetworkTableEntry? = null
@@ -37,110 +33,17 @@ var distanceEntry: NetworkTableEntry? = null
 
 fun main(args: Array<String>) = runBlocking {
     var webcam: Webcam? = null
-    var connected = false
-    while (!connected) {
-        try {
-
-            if (args.size != 1) {
-                remote = true
-                Webcam.setDriver(IpCamDriver())
-                val checkServer = IpCamDeviceRegistry.register("RoboRioCamTest", robotAddr, IpCamMode.PUSH)
-                while (!checkServer.isOnline) {
-                    println("Waiting on Server")
-                    sleep(250)
-                }
-                IpCamDeviceRegistry.register("RoboRioCam", "$robotAddr:1181/stream.mjpg", IpCamMode.PUSH)
-            }
-
-            val webcams = Webcam.getWebcams()
-            webcams
-                .map { it.name }
-                .forEachIndexed { i, cam -> println("$i: $cam") }
-
-            webcam = webcams.first()
-
-
-            webcam.setCustomViewSizes(Dimension(640, 480))
-            webcam.viewSize = Dimension(640, 480)
-            connected = webcam.open()
-        } catch (ex: Exception) {
-            println("could not connect")
-            sleep(250)
-        }
+    isRemote = args.size != 1
+    while (webcam?.isOpen != true) {
+        webcam = connect()
     }
 
     println("connected")
 
     val panel = WebcamPanel(webcam)
+    panel.painter = getPainter()
 
-    panel.painter = object : WebcamPanel.Painter {
-        override fun paintPanel(panel: WebcamPanel, g2: Graphics2D) {
-
-        }
-
-        override fun paintImage(panel: WebcamPanel, image: BufferedImage, g2: Graphics2D) {
-            val imageToUse = if (testImage != null) ImageIO.read(File(testImage)) else image
-
-            // Lets You Pick what team you are on "red" or "blue"
-            val targetingOutput = if (teamColor == "blue") {
-                 process(Color.GREEN, imageToUse) { r, g, b ->
-                    b > 75 && b > r + 45 && b > g + 45
-                }
-            } else { // red
-                 process(Color.GREEN, imageToUse) { r, g, b ->
-                    r > 95 && r > b + 65 && r > g + 65
-                }
-            }
-
-            // pull out results we care about, let web server serve them as quick as possible
-            imageInfo = ImageInfo(
-                seen = targetingOutput.seen,
-                centerX = targetingOutput.targetXCenter,
-                centerY = targetingOutput.targetYCenter,
-                distance = min(1000.0, targetingOutput.targetDistanceInches)
-            )
-
-
-            fullSend()
-
-            writeToPanel(panel, g2, targetingOutput)
-        }
-
-        /**
-         * Writes an image onto the panel, and deals with stretching it while keeping aspect ratio
-         * @param panel
-         * @param g2
-         * @param targetingOutput
-         */
-        private fun writeToPanel(panel: WebcamPanel, g2: Graphics2D, targetingOutput: TargetingOutput) {
-
-            // Draw our results onto the image, so that the driver can see if the autonomous code is tracking
-            val outImage = targetingOutput.drawOntoImage(Color.RED, targetingOutput.processedImage)
-
-            val imageX = outImage.width
-            val imageY = outImage.height
-
-            val imageAspectRatio = imageX.toFloat() / imageY.toFloat()
-
-            val panelX = panel.width
-            val panelY = panel.height
-
-            val screenAspectRatio = panelX.toFloat() / panelY.toFloat()
-
-            if (imageAspectRatio < screenAspectRatio) {
-                // wide screen - y to the max
-                val scaledImageX = (panelY * imageAspectRatio).toInt()
-                g2.drawImage(outImage, (panelX - scaledImageX) / 2, 0, scaledImageX, panelY, null)
-
-            } else {
-                // tall screen - x to the max
-                val scaledImageY = (panelX / imageAspectRatio).toInt()
-                g2.drawImage(outImage, 0, (panelY - scaledImageY) / 2, panelX, scaledImageY, null)
-            }
-        }
-    }
-
-    val window = JFrame("Test webcam panel")
+    val window = JFrame("Webcam Panel")
     window.add(panel)
     window.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
     window.pack()
@@ -148,61 +51,131 @@ fun main(args: Array<String>) = runBlocking {
 
     window.addKeyListener(ToggleColor())
 
-    // handle exceptions by just printing them out
-    exception(Exception::class.java) { exception, request, response -> exception.printStackTrace() }
-
     val inst = NetworkTableInstance.getDefault()
     val table = inst.getTable("datatable")
-
     seenEntry = table.getEntry("seen")
     centerXEntry = table.getEntry("centerX")
     centerYEntry = table.getEntry("centerY")
     distanceEntry = table.getEntry("distance")
-
-    // a little webserver.  Go to http://localhost:4567/center
-
-//    port(5801)
-//    get("/center") { req, res ->
-//
-//        imageInfo.seen = req.queryParams("seen").toBoolean()
-//        imageInfo.center = req.queryParams("center").toDouble()
-//        imageInfo.distance = req.queryParams("distance").toDouble()
-//
-//        "ok $imageInfo"
-//    }
-
-    // This will be accessed from the robot at
-    // http://laptop-1091.local:4567/center
-
 }
 
-fun fullSend() {
+private fun getPainter() = object : WebcamPanel.Painter {
+    override fun paintPanel(panel: WebcamPanel, g2: Graphics2D) {}
 
-    if (remote) {
+    override fun paintImage(panel: WebcamPanel, image: BufferedImage, g2: Graphics2D) {
+        val imageToUse = if (testImage != null) ImageIO.read(File(testImage)) else image
 
+        // Lets You Pick what team you are on "red" or "blue"
+        val targetingOutput = getTargetingOutput(imageToUse)
+
+        // pull out results we care about, let web server serve them as quick as possible
+        val imageInfo = ImageInfo(
+            seen = targetingOutput.seen,
+            centerX = targetingOutput.targetXCenter,
+            centerY = targetingOutput.targetYCenter,
+            distance = min(1000.0, targetingOutput.targetDistanceInches)
+        )
+
+        sendToNetworkTables(imageInfo)
+
+        writeToPanel(panel, g2, targetingOutput)
+    }
+
+    private fun getTargetingOutput(imageToUse: BufferedImage) =
+        if (teamColor == "blue") {
+            process(Color.GREEN, imageToUse) { r, g, b ->
+                b > 75 && b > r + 45 && b > g + 45
+            }
+        } else { // red
+            process(Color.GREEN, imageToUse) { r, g, b ->
+                r > 95 && r > b + 65 && r > g + 65
+            }
+        }
+
+    /**
+     * Writes an image onto the panel, and deals with stretching it while keeping aspect ratio
+     * @param panel
+     * @param g2
+     * @param targetingOutput
+     */
+    /**
+     * Writes an image onto the panel, and deals with stretching it while keeping aspect ratio
+     * @param panel
+     * @param g2
+     * @param targetingOutput
+     */
+    private fun writeToPanel(panel: WebcamPanel, g2: Graphics2D, targetingOutput: TargetingOutput) {
+
+        // Draw our results onto the image, so that the driver can see if the autonomous code is tracking
+        val outImage = targetingOutput.drawOntoImage(Color.RED, targetingOutput.processedImage)
+
+        val imageX = outImage.width
+        val imageY = outImage.height
+
+        val imageAspectRatio = imageX.toFloat() / imageY.toFloat()
+
+        val panelX = panel.width
+        val panelY = panel.height
+
+        val screenAspectRatio = panelX.toFloat() / panelY.toFloat()
+
+        if (imageAspectRatio < screenAspectRatio) {
+            // wide screen - y to the max
+            val scaledImageX = (panelY * imageAspectRatio).toInt()
+            g2.drawImage(outImage, (panelX - scaledImageX) / 2, 0, scaledImageX, panelY, null)
+
+        } else {
+            // tall screen - x to the max
+            val scaledImageY = (panelX / imageAspectRatio).toInt()
+            g2.drawImage(outImage, 0, (panelY - scaledImageY) / 2, panelX, scaledImageY, null)
+        }
+    }
+}
+
+private fun connect(): Webcam? {
+    var webcam: Webcam? = null
+    try {
+
+        if (isRemote) {
+            Webcam.setDriver(IpCamDriver())
+            val checkServer = IpCamDeviceRegistry.register("RoboRioCamTest", robotAddr, IpCamMode.PUSH)
+            while (!checkServer.isOnline) {
+                println("Waiting on Server")
+                sleep(250)
+            }
+            IpCamDeviceRegistry.register("RoboRioCam", "$robotAddr:1181/stream.mjpg", IpCamMode.PUSH)
+        }
+
+        val webcams = Webcam.getWebcams()
+        webcams
+            .map { it.name }
+            .forEachIndexed { i, cam -> println("$i: $cam") }
+
+        webcam = webcams.first()
+        webcam.setCustomViewSizes(Dimension(640, 480))
+        webcam.viewSize = Dimension(640, 480)
+        webcam.open();
+    } catch (ex: Exception) {
+        println("could not connect")
+        sleep(250)
+    }
+    return webcam
+}
+
+fun sendToNetworkTables(imageInfo: ImageInfo) {
+    if (isRemote) {
         seenEntry?.setBoolean(imageInfo.seen)
         centerXEntry?.setDouble(imageInfo.centerX)
         centerYEntry?.setDouble(imageInfo.centerY)
         distanceEntry?.setDouble(imageInfo.distance)
-
-
-//        table.putValue()
-        //run {
-        //if its new,
-        //URL("$robotAddr:5801/center?seen=${imageInfo.seen}&distance=${imageInfo.distance}&center=${imageInfo.center}").readText()
-        //
     }
 }
 
-//val pixelRange = 0..
-
 fun process(
-    targetColor: Color,
+    targetDisplayColor: Color,
     inputImage: BufferedImage,
     isColor: (r: Int, g: Int, b: Int) -> Boolean
 ): TargetingOutput {
-
-
     val outputImage = BufferedImage(
         inputImage.width, inputImage.height,
         BufferedImage.TYPE_INT_RGB
@@ -215,7 +188,6 @@ fun process(
     var totalCount = 0.0
 
     // critical code, run on every pixel, every frame
-
     for (y in 0 until inputImage.height) {
         for (x in 0 until inputImage.width) {
             val xMult = 1 - abs((x.toDouble() / inputImage.width) - 0.5)
@@ -230,7 +202,7 @@ fun process(
             val b = rgb and 0x000000FF
 
             if (isColor(r, g, b)) {
-                outputImage.setRGB(x, y, targetColor.rgb)
+                outputImage.setRGB(x, y, targetDisplayColor.rgb)
                 xSum += x * xMult
                 ySum += y * xMult
                 totalCount += xMult
@@ -241,16 +213,12 @@ fun process(
         }
     }
 
-
     val xCenter: Int
     val yCenter: Int
 
     var seen = false
-    var pixelSize = 0
     var rightExtension = inputImage.width
     var leftExtension = inputImage.width
-    var topExtension = inputImage.height
-    var bottomExtension = inputImage.height
 
     if (totalCount <= 50) {
         xCenter = inputImage.width / 2
@@ -261,7 +229,7 @@ fun process(
 
         // left
         for (x in (xCenter downTo 0)) {
-            if (outputImage.getRGB(x, yCenter) != targetColor.rgb) {
+            if (outputImage.getRGB(x, yCenter) != targetDisplayColor.rgb) {
                 leftExtension = xCenter - x
                 break
             }
@@ -269,7 +237,7 @@ fun process(
 
         // right
         for (x in xCenter until inputImage.width) {
-            if (outputImage.getRGB(x, yCenter) != targetColor.rgb) {
+            if (outputImage.getRGB(x, yCenter) != targetDisplayColor.rgb) {
                 rightExtension = x - xCenter
                 break
             }
@@ -278,19 +246,8 @@ fun process(
         val total = rightExtension + leftExtension
         if (total < inputImage.width) {
             seen = true
-            pixelSize = total
         }
     }
-
-    /*
-    * distance(mm) = (focal length (mm) * real height of the object (mm) * camera frame height in device (pixels) ) / ( image height (pixels) * sensor height (mm))
-    * */
-    //In Millimeters - Values for C270 webcam
-  //  val focalLength = 4.2
-  //  val targetPhysicalHeight = 279.4 //11in
-    val cameraFrameHeight = inputImage.height
-    val cameraSensorHeight = 2.2
-    val targetPixelHeight = pixelSize //How to get?
 
     val xLeft = xCenter - leftExtension
     val xRight = xCenter + rightExtension
